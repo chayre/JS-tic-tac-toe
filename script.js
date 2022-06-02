00c3fa88a742288a9da2311adc175a96e9c4cb07
seairs <sea.airs77@gmail.com>
2022-06-02 02:11:36 -0400

    swaps between players after clicking; checks for 1 win condition and fills other rows when fulfilled. need to add other win conditions.

--- script.js
+++ script.js
@@ -19,9 +19,13 @@ const gameboard = (() => {
 		for(let i = 0; i < 9; i++) {
 			if (board[i] == '') {
 				board[i] = '-'	
-			}
-			
+			}	
 		}
+		displayController.boxes.forEach((box) => {
+			if (box.innerHTML.length == 0) {
+					box.innerHTML = '-';
+			} 
+		});
 	};
 	
 	// public functions
@@ -44,124 +48,47 @@ const displayController = (() => {
 	let bottom_left = document.getElementById('bottom-left');
 	let bottom_center = document.getElementById('bottom-center');
 	let bottom_right = document.getElementById('bottom-right');
+    let boxes = document.querySelectorAll('div.ticelement');
 
 	// store div elements in state array
 	let state = [top_left, top_center, top_right,
 				 middle_left, middle_center, middle_right,
 				 bottom_left, bottom_center, bottom_right]
 
-	// boolean to know if the eventListeners have been added
-    let listening = false;
-
 	// add an eventListener to each div in the state array if it has no innerHTML; when clicked, add the n input to the gameboard and update the display. 
 	const listen = (n) => { 
-		/*listening = true;
-		for (i = 0; i < state.length; i++) {
-			if (state[i].innerHTML.length == 0) {
-				let tmp = i;
-				// the EventListener will only function once
-				state[tmp].addEventListener("click", function () {
-					gameboard.add(tmp, n);
-					update();
-					listening = false;
-					console.log(turn)
-					game.play();
-				}, {once:true})
-			}
-		}*/
-		document.addEventListener('click', function (event) {
-
-			if (event.target.matches('.top-left')) {
-				gameboard.add(0, n);
+		boxes.forEach((box) => {
+			box.addEventListener('click', () => {
+				if (turn % 2 == 0 && box.innerHTML.length == 0) {
+					box.innerHTML = 'X';
+				} else if (turn % 2 == 1 && box.innerHTML.length == 0) {
+					box.innerHTML = 'O'
+				}
 				turn++;
 				update();
-				game.play();
-			} 
-
-			if (event.target.matches('.top-center')) {
-				gameboard.add(1, n);
-				turn++;
-				update();
-				game.play();
-			}
-			if (event.target.matches('.top-right')) {
-				gameboard.add(2, n);
-				turn++;
-				update();
-				game.play();
-			}
-			if (event.target.matches('.middle-left')) {
-				gameboard.add(3, n);
-				turn++;
-				update();
-				game.play();
-			}
-			if (event.target.matches('.middle-center')) {
-				gameboard.add(4, n);
-				turn++;
-				update();
-				game.play();
-			}
-			if (event.target.matches('.middle-right')) {
-				gameboard.add(5, n);
-				turn++;
-				update();
-				game.play();
-			}
-			if (event.target.matches('.bottom-left')) {
-				gameboard.add(6, n);
-				turn++;
-				update();
-				game.play();
-			}
-			if (event.target.matches('.bottom-center')) {
-				gameboard.add(7, n);
-				turn++;
-				update();
-				game.play();
-			}
-			if (event.target.matches('.top-right')) {
-				gameboard.add(8, n);
-				turn++;
-				update();
-				game.play();
-			}
-		}, {once:true});
-
-
+				console.log(gameboard.board)
+				game.checkWinner();
+			});
+		  }, { once: true });
 	};
-
-
-	
-
-
-
-
-
-
+		
 	// holds the turn
 	let turn = 1;
 
-	// allow other modules to add to turn
-	const turnAdd = () => { 
-		turn++;
-	};
-
-	// update the DOM to reflect the board array
+	// update the board array to reflect DOM
 	const update = () => { 
 		for (i = 0; i < state.length; i++) {
-			state[i].innerHTML = gameboard.board[i];
+			gameboard.board[i] = state[i].innerHTML
 		}
 	};
 	
 	// public functions
 	return {
 	  update,
-	  listening,
 	  listen,
 	  state,
 	  turn,
-	  turnAdd
+	  boxes
 	};
 })();
 
@@ -174,16 +101,26 @@ const game = (() => {
 				gameboard.finished();
 				return true;
 			}
+
+
+			// Add more win conditions here. add function to change color of winning row
+
 		}
 	};
 
+
+
+	// move functions from listen to play
+
 	const play = () => { 
+		displayController.listen
 		if (displayController.turn % 2 == 0) {
-			displayController.listen('O');
-			displayController.turnAdd();
+	
+			console.log(displayController.turn)
+			//displayController.turnAdd();
 		} else {
-			displayController.listen('X');
-			displayController.turnAdd();
+			//displayController.turnAdd();
+			console.log(displayController.turn)
 		}
 	};
 	
@@ -194,4 +131,4 @@ const game = (() => {
 	};
 })();
 
-game.play();
+displayController.listen('X')
