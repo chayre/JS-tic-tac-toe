f64f0805c848d73be3d90692e70280a2767f623b
seairs <sea.airs77@gmail.com>
2022-06-01 23:53:18 -0400

    added comments, made DOM elements private (helpers)

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 const gameboard = (() => {
+	// initialize gameboard as empty array of 9 elements
 	let board = ['','','',
 				   '','','',
 				   '','',''];
 
+	// allows you to swap out an element at board index a for input b
 	const add = (a, b) => board[a] = b;
 	
+	// take the board back to the initialized state
 	const reset = () => { 
 		for(let i = 0; i < 9; i++) {
 			board[i] = ''
 		}
 	};
 	
+	// public functions
 	return {
 	  board,
 	  add,
@@ -19,43 +23,46 @@ const gameboard = (() => {
 })();
 
 const displayController = (() => {
+	// pull div elements from DOM
 	let top_left = document.getElementById('top-left');
 	let top_center = document.getElementById('top-center');
 	let top_right = document.getElementById('top-right');
-	
 	let middle_left = document.getElementById('middle-left');
 	let middle_center = document.getElementById('middle-center');
 	let middle_right = document.getElementById('middle-right');
-	
 	let bottom_left = document.getElementById('bottom-left');
 	let bottom_center = document.getElementById('bottom-center');
 	let bottom_right = document.getElementById('bottom-right');
 
+	// store div elements in state array
 	let state = [top_left, top_center, top_right,
 				 middle_left, middle_center, middle_right,
 				 bottom_left, bottom_center, bottom_right]
 
+	// add an eventListener to each div in the state array if it has no innerHTML; when clicked, add the n input to the gameboard and update the display. 
 	const listen = (n) => { 
 		for (i = 0; i < state.length; i++) {
 			if (state[i].innerHTML.length == 0) {
 				let tmp = i;
+				// the EventListener will only function once
 				state[i].addEventListener("click", function () {
 					gameboard.add(tmp, n);
-					console.log(tmp)
-					displayController.update();
+					update();
 				}, {once:true})
 			}
 		}
 	};
 
+	// update the DOM to reflect the board array
 	const update = () => { 
 		for (i = 0; i < state.length; i++) {
 			state[i].innerHTML = gameboard.board[i];
 		}
 	};
 	
+	// public functions
 	return {
-	  top_left,
+	  /*top_left,
 	  top_center,
 	  top_right,
 	  middle_left,
@@ -63,7 +70,7 @@ const displayController = (() => {
 	  middle_right,
 	  bottom_left,
 	  bottom_center,
-	  bottom_right,
+	  bottom_right,*/
 	  update,
 	  listen,
 	  state
