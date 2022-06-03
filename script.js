66f768e720c115ecceb28b74df3f648cb9daaba1
Colin Ayres <cayres7@gmail.com>
2022-06-02 22:26:11 -0400

    added congratulatory message to winner

--- script.js
+++ script.js
@@ -55,6 +55,10 @@ const displayController = (() => {
 		gameinfo.innerHTML = "Turn " + turn +"; your turn, " + marker + "."
 	}
 
+	let congratulate = (turn, marker) => {
+		gameinfo.innerHTML = "Well done " + marker +", you won on turn " + turn + "."
+	}
+
 	// store div elements in state array
 	let state = [top_left, top_center, top_right,
 				 middle_left, middle_center, middle_right,
@@ -74,10 +78,12 @@ const displayController = (() => {
 						marker = 'X'
 					}
 					update();
-					console.log(gameboard.board)
-					game.checkWinner()
-					turn++;
-					updateInfo(turn, marker);
+					if(game.checkWinner()) {
+						congratulate(turn, box.innerHTML);
+					} else {
+						turn++;
+						updateInfo(turn, marker);
+					}
 				}
 			});
 		  }, { once: true });
@@ -99,7 +105,8 @@ const displayController = (() => {
 	  listen,
 	  state,
 	  turn,
-	  boxes
+	  boxes,
+	  updateInfo
 	};
 })();
 
@@ -107,8 +114,7 @@ const game = (() => {
 	// take the board back to the initialized state
 	const checkWinner = () => { 
 		for (i = 0; i < 7; i+=3) {
-			if ((gameboard.board[i] != '') && gameboard.board[i] === gameboard.board[i+1] && gameboard.board[i] === gameboard.board[i+2]) {
-				console.log(displayController.boxes[i]);
+			if ((gameboard.board[i] != '' && gameboard.board[i] != '-') && gameboard.board[i] === gameboard.board[i+1] && gameboard.board[i] === gameboard.board[i+2]) {
 				displayController.boxes[i].classList.add("green");
 				displayController.boxes[i+1].classList.add("green");
 				displayController.boxes[i+2].classList.add("green");
@@ -117,33 +123,31 @@ const game = (() => {
 			}
 		}
 		for (i = 0; i < 3; i++) {
-			if ((gameboard.board[i] != '') && gameboard.board[i] === gameboard.board[i+3] && gameboard.board[i] === gameboard.board[i+6]) {
-				console.log(gameboard.board[i]);
-				gameboard.boxes[i].classList.add("green");
-				gameboard.boxes[i+3].classList.add("green");
-				gameboard.boxes[i+3].classList.add("green");
+			if ((gameboard.board[i] != '' && gameboard.board[i] != '-') && gameboard.board[i] === gameboard.board[i+3] && gameboard.board[i] === gameboard.board[i+6]) {
+				displayController.boxes[i].classList.add("green");
+				displayController.boxes[i+3].classList.add("green");
+				displayController.boxes[i+3].classList.add("green");
 				gameboard.finished();
+				displayController.update();
 				return true;
 			}
 		}
-		if ((gameboard.board[0] != '') && gameboard.board[0] === gameboard.board[4] && gameboard.board[0] === gameboard.board[8]) {
-			console.log(gameboard.board[i]);
-			gameboard.boxes[0].classList.add("green");
-			gameboard.boxes[4].classList.add("green");
-			gameboard.boxes[8].classList.add("green");
+		if ((gameboard.board[0] != '' && gameboard.board[0] != '-') && gameboard.board[0] === gameboard.board[4] && gameboard.board[0] === gameboard.board[8]) {
+			displayController.boxes[0].classList.add("green");
+			displayController.boxes[4].classList.add("green");
+			displayController.boxes[8].classList.add("green");
 			gameboard.finished();
 			return true;
 		}
 
-		if ((gameboard.board[0] != '') && gameboard.board[0] === gameboard.board[4] && gameboard.board[0] === gameboard.board[8]) {
+		if ((gameboard.board[2] != '' && gameboard.board[2] != '-') && gameboard.board[2] === gameboard.board[4] && gameboard.board[2] === gameboard.board[6]) {
 			console.log(gameboard.board[i]);
-			gameboard.boxes[2].classList.add("green");
-			gameboard.boxes[4].classList.add("green");
-			gameboard.boxes[6].classList.add("green");
-			gameboard.finished();
+			displayController.boxes[2].classList.add("green");
+			displayController.boxes[4].classList.add("green");
+			displayControllerboxes[6].classList.add("green");
+			displayController.finished();
 			return true;
 		}
-		
 	};
 
 
